630308e90a0bc8d0330a001d37d2cf2e0803a70e
losbiw <skorinov41@gmail.com>
2021-02-06 11:56:54 +0200

    Replace dev server with static webapp

--- QuasarMessenger-desktop/src/electron-main.js
+++ QuasarMessenger-desktop/src/electron-main.js
@@ -945,9 +945,10 @@ app.on('ready', async () => {
 
     let webUrl = 'vector://vector/webapp/';
 
-    if(process.env.NODE_ENV !== 'production'){
-        webUrl = 'http://127.0.0.1:8080/';
-    }
+    //dev-only
+    // if(process.env.NODE_ENV !== 'production'){
+    //     webUrl = 'http://127.0.0.1:8080/';
+    // }
 
     mainWindow.loadURL(webUrl);
     Menu.setApplicationMenu(vectorMenu);
