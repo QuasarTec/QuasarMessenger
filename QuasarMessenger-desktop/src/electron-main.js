f0c2f04bf3807590b7f971abc3c84f7dbaebb04b
losbiw <skorinov41@gmail.com>
2021-02-08 17:54:19 +0200

    Change url for dev mode

--- QuasarMessenger-desktop/src/electron-main.js
+++ QuasarMessenger-desktop/src/electron-main.js
@@ -949,7 +949,7 @@ app.on('ready', async () => {
 
     let webUrl = 'vector://vector/webapp/';
 
-    if(process.env.NODE_ENV !== 'production'){
+    if(!app.isPackaged){
         webUrl = 'http://127.0.0.1:8080/';
     }
 
