4e717e1e99e01028193d0f7b9e1f65903ec7d5ca
losbiw <skorinov41@gmail.com>
2021-07-17 00:17:46 +0300

    Add wikipedia button

--- matrix-react-sdk/src/components/structures/browser/DefaultLinks.tsx
+++ matrix-react-sdk/src/components/structures/browser/DefaultLinks.tsx
@@ -27,6 +27,12 @@ const buttons: Button[] = [
         name: 'Connect',
     },
     {
+      // eslint-disable-next-line max-len
+      url: 'https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0',
+      iconPath: require('../../../../res/img/links/wikipedia.png'),
+      name: 'Wikipedia',
+    },
+    {
         url: 'https://rucaptcha.com/',
         iconPath: require('../../../../res/img/links/rucaptcha.png'),
         name: 'ruCaptcha',
